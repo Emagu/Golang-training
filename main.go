a7fef50ea4b079b0bd72e5810d41a7ee88722aa6
emagu <emagudev@gmail.com>
2022-09-02 18:53:39 +0800

    change page

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"strings"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析參數，預設是不會解析的
+	fmt.Println(r.Form) //這些資訊是輸出到伺服器端的列印資訊
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
 	}
+	fmt.Fprintf(w, "Hello astaxie!") //這個寫入到 w 的是輸出到客戶端的
+}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+func main() {
+	http.HandleFunc("/", sayhelloName)       //設定存取的路由
+	err := http.ListenAndServe(":9090", nil) //設定監聽的埠
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
